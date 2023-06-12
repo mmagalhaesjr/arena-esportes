e84d31150b403d52a7ff86c6868a999ad75e25b2
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2023-06-12 17:19:31 -0300

    feat: cadastro/login

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,24 +3,30 @@ import TelaQuadras from "./pages/TelaQuadras/TelaQuadras";
 import TelaReservas from "./pages/TelaReservas/TelaReservas";
 import TelaCadastro from "./pages/TelaCadastro/TelaCadastro";
 import TelaLogin from "./pages/TelaLogin/TelaLogin";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserProvider from "./contexts/UserContext";
+
+
 
 export default function App() {
-  
+
   return (
-   
-        <BrowserRouter>
-          <div className="App">
-            <Routes>
-              <Route path="/" element={<TelaInicial />} />
-              <Route path="/cadastro" element={<TelaCadastro />} />
-              <Route path="/login" element={<TelaLogin />} />
-              <Route path="/quadras" element={<TelaQuadras />} />
-              <Route path="/reservas" element={<TelaReservas />} />
-            </Routes>
-          </div>
-        </BrowserRouter>
-    
+
+    <BrowserRouter>
+
+      <UserProvider>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<TelaInicial />} />
+            <Route path="/cadastro" element={<TelaCadastro />} />
+            <Route path="/login" element={<TelaLogin />} />
+            <Route path="/quadras" element={<TelaQuadras />} />
+            <Route path="/reservas" element={<TelaReservas />} />
+          </Routes>
+        </div>
+      </UserProvider>
+
+    </BrowserRouter>
+
   )
 }
--- front-end/src/pages/TelaLogin/TelaLogin.jsx
+++ front-end/src/pages/TelaLogin/TelaLogin.jsx
@@ -1,34 +1,74 @@
-import { Link } from 'react-router-dom';
-import Corpo from '../../components/corpo/Corpo'
-import { StyledLogin } from "./styled"
+import Corpo from '../../components/corpo/Corpo';
+import { StyledLogin } from "./styled";
+import { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+
+import { useContext } from 'react';
+import { UserContext } from '../../contexts/UserContext'
 
 
 
 
-function CadastroUsuario() {
+function LoginUsuario() {
+  const [email, setEmail] = useState('')
+  const [senha, setSenha] = useState('')
+  
+  const {token , setToken} = useContext(UserContext)
 
+
+  const navigate = useNavigate()
+
+  function logar(e) {
+    e.preventDefault();
+
+
+    const body = {
+      email: email,
+      senha: senha
+    };
+
+
+    const url = `${import.meta.env.VITE_API_BASE_URL}/sign-in`;
+    const promise = axios.post(url, body);
+
+    promise.then((res) => {
+      navigate('/');
+      
+      setToken(res.data.token)
+      localStorage.setItem("token",token)
+
+    });
+
+    promise.catch(err => {
+      alert(err.response.data);
+    });
+  }
 
   return (
     <>
       <Corpo />
       <StyledLogin>
-        <form action="">
+        <form onSubmit={logar}>
           <br />
           <div className="inputBox">
-            <input type="text" name="email" id="email" className="inputUser" required />
+            <input type="text" name="email" id="email" className="inputUser" required
+              value={email} onChange={e => setEmail(e.target.value)} />
             <label htmlFor="email" className="labelInput">Email</label>
           </div>
 
           <br /><br />
 
           <div className="inputBox">
-            <input type="tel" name="senha" id="senha" className="inputUser" required />
+            <input type="tel" name="senha" id="senha" className="inputUser" required
+              value={senha} onChange={e => setSenha(e.target.value)} />
             <label htmlFor="senha" className="labelInput">Senha</label>
           </div>
 
           <br /><br />
 
-          <input type="submit" name="submit" id="submit" />
+          <button type="submit" name="submit" id="submit">Entrar</button>
+
           <br /><br />
         </form>
         <Link to="/cadastro">Cadastre-se</Link>
@@ -39,4 +79,4 @@ function CadastroUsuario() {
   );
 }
 
-export default CadastroUsuario;
+export default LoginUsuario;
